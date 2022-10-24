b4c0a5f97b3b79af0031447a39bb51a6c7415e6a
k.sudharshan <kamavaramsudharshan1706@gmail.com>
2022-10-24 13:14:16 +0530

    pagination

---
+++ pagination.js
@@ -0,0 +1,127 @@
+// fetch("https://gist.githubusercontent.com/rvsp/add40254aa126f045837fa5b51f47f1f/raw/4d724bfabf4cce7379a386e23bef6576ab99a2f9/pagination.json")
+// .then((response)=>{
+//     console.log(response)
+//     return response.json()
+// }
+// )
+// .then((data)=>{
+//     console.log(data)
+// })
+
+var globalData = []
+var currentidx = 0
+const table = document.createElement('table');
+table.className = 'table table-bordered'
+const thead = document.createElement('thead');
+const trele = document.createElement('tr');
+['id','name','email'].forEach((columnname)=>{
+    const hele = document.createElement('th');
+    hele.innerText =columnname;
+    trele.appendChild(hele);
+})
+thead.append(trele);
+// tr.style.border = "1px solid"
+const tbody = document.createElement('tbody')
+table.append(thead,tbody);
+
+const btnsDiv = document.createElement('div');
+const btns = [
+    {type:"button",name:"prev",id:"prev"},
+    {type:"button",name:"next",id:"next"}
+]
+
+btns.forEach((btn)=>{
+    const newBtn = document.createElement('button');
+    newBtn.id = btn.id;
+    if(btn.name == "next"){
+        newBtn.innerText = "Next";
+        newBtn.setAttribute('onClick',"funnext()")
+    }
+    else{
+        newBtn.innerText = "Prev";
+        newBtn.setAttribute('onClick',"funprev()")
+
+    }
+    console.log(newBtn.innerText)
+    newBtn.style.width = '100px'
+    btnsDiv.append(newBtn);  
+
+})
+
+document.body.append(table,btnsDiv)
+document.body.style.textAlign = "center"
+fetch("https://gist.githubusercontent.com/rvsp/add40254aa126f045837fa5b51f47f1f/raw/4d724bfabf4cce7379a386e23bef6576ab99a2f9/pagination.json")
+.then((response)=>{
+    console.log(response)
+    
+    return response.json()
+}
+)
+.then((data)=>{
+    globalData = data
+    data.slice(0,5).forEach(({id,name,email})=>{
+        const innertr = document.createElement('tr');
+        const innerid = document.createElement('td');
+        innerid.innerText = id
+        const innername = document.createElement('td');
+        innername.innerText = name
+        const inneremail = document.createElement('td');
+        inneremail.innerText = email
+        innertr.append(innerid,innername,inneremail);
+        tbody.append(innertr)
+    })  
+})
+
+const funnext = ()=> {
+    
+    currentidx += 5
+    // currentidx = currentidx*5
+    var nextidx = currentidx + 5
+    console.log(currentidx,nextidx)
+    if(currentidx < 100){
+        tbody.innerText = ""
+    globalData.slice(currentidx,nextidx).forEach(({id,name,email})=>{
+      
+        const innertr = document.createElement('tr');
+        const innerid = document.createElement('td');
+        innerid.innerText = id
+        const innername = document.createElement('td');
+        innername.innerText = name
+        const inneremail = document.createElement('td');
+        inneremail.innerText = email
+        innertr.append(innerid,innername,inneremail);
+        tbody.append(innertr)
+    })    
+}
+else{
+    currentidx = 95
+}
+}
+
+const funprev = ()=> {
+    
+    currentidx -= 5
+    // currentidx = currentidx*5
+    var nextidx = currentidx + 5
+    console.log(currentidx,nextidx)
+    console.log("here")
+    if(currentidx >= 0){
+        tbody.innerText = ""
+        console.log("here")
+        globalData.slice(currentidx,nextidx).forEach(({id,name,email})=>{
+      
+        const innertr = document.createElement('tr');
+        const innerid = document.createElement('td');
+        innerid.innerText = id
+        const innername = document.createElement('td');
+        innername.innerText = name
+        const inneremail = document.createElement('td');
+        inneremail.innerText = email
+        innertr.append(innerid,innername,inneremail);
+        tbody.append(innertr)
+    })
+    }
+    else{
+        currentidx = 0;
+    }
+}
