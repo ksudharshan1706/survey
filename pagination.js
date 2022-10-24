9137f7d18a04c9188d0a05cc32824664a42b13e5
k.sudharshan <kamavaramsudharshan1706@gmail.com>
2022-10-24 13:50:11 +0530

    update

--- pagination.js
+++ pagination.js
@@ -1,15 +1,8 @@
-// fetch("https://gist.githubusercontent.com/rvsp/add40254aa126f045837fa5b51f47f1f/raw/4d724bfabf4cce7379a386e23bef6576ab99a2f9/pagination.json")
-// .then((response)=>{
-//     console.log(response)
-//     return response.json()
-// }
-// )
-// .then((data)=>{
-//     console.log(data)
-// })
-
 var globalData = []
 var currentidx = 0
+const mainDiv = document.createElement('div')
+mainDiv.classList.add("container")
+
 const table = document.createElement('table');
 table.className = 'table table-bordered'
 const thead = document.createElement('thead');
@@ -20,11 +13,11 @@ const trele = document.createElement('tr');
     trele.appendChild(hele);
 })
 thead.append(trele);
-// tr.style.border = "1px solid"
 const tbody = document.createElement('tbody')
 table.append(thead,tbody);
 
 const btnsDiv = document.createElement('div');
+
 const btns = [
     {type:"button",name:"prev",id:"prev"},
     {type:"button",name:"next",id:"next"}
@@ -33,6 +26,7 @@ const btns = [
 btns.forEach((btn)=>{
     const newBtn = document.createElement('button');
     newBtn.id = btn.id;
+    newBtn.style.margin = "20px"
     if(btn.name == "next"){
         newBtn.innerText = "Next";
         newBtn.setAttribute('onClick',"funnext()")
@@ -42,18 +36,18 @@ btns.forEach((btn)=>{
         newBtn.setAttribute('onClick',"funprev()")
 
     }
-    console.log(newBtn.innerText)
     newBtn.style.width = '100px'
     btnsDiv.append(newBtn);  
 
 })
-
-document.body.append(table,btnsDiv)
+table.style.justifyContent="center"
+table.style.textAlign = "center"
+mainDiv.append(table,btnsDiv)
+mainDiv.style.textAlign ="center"
+document.body.append(mainDiv)
 document.body.style.textAlign = "center"
 fetch("https://gist.githubusercontent.com/rvsp/add40254aa126f045837fa5b51f47f1f/raw/4d724bfabf4cce7379a386e23bef6576ab99a2f9/pagination.json")
 .then((response)=>{
-    console.log(response)
-    
     return response.json()
 }
 )
@@ -77,7 +71,6 @@ const funnext = ()=> {
     currentidx += 5
     // currentidx = currentidx*5
     var nextidx = currentidx + 5
-    console.log(currentidx,nextidx)
     if(currentidx < 100){
         tbody.innerText = ""
     globalData.slice(currentidx,nextidx).forEach(({id,name,email})=>{
